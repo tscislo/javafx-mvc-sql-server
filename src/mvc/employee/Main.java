af3b06569f44ee6912cbeefcfea2ec6f8e6b346f
Tomasz Ścisło <scislot@gmail.com>
2019-02-09 13:49:38 +0100

    main controller

--- src/mvc/employee/Main.java
+++ src/mvc/employee/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import mvc.employee.controller.MainController;
 import mvc.employee.model.dal.OraConn;
 
 public class Main extends Application {
@@ -24,20 +25,22 @@ public class Main extends Application {
         primaryStage.setMinHeight(height);
         primaryStage.setMaxWidth(width);
         primaryStage.setMaxHeight(height);
-//        MainController mainWindowController = fxmlLoader.getController();
-//        mainWindowController.setStage(primaryStage);
+        MainController mainWindowController = fxmlLoader.getController();
+        mainWindowController.setPrimaryStage(primaryStage);
         primaryStage.show();
 
-        OraConnInstance = new OraConn("jdbc:oracle:thin:@ora3.elka.pw.edu.pl:1521:ora3inf", "TEMP0", "temp01" );
+        this.initializeConnection();
+        System.out.print("OK" );
+    }
+
+    private void initializeConnection() {
+        OraConnInstance = new OraConn("jdbc:oracle:thin:@ora3.elka.pw.edu.pl:1521:ora3inf", "TEMP01", "temp01" );
         try {
             OraConnInstance.open();
         } catch (Exception ex) {
             Alert.showError(ex.getMessage());
         }
-        System.out.print("OK" );
-
     }
-
 
     public static void main(String[] args) {
         launch(args);
--- src/mvc/employee/controller/MainController.java
+++ src/mvc/employee/controller/MainController.java
@@ -1,13 +1,28 @@
 package mvc.employee.controller;
 
 import javafx.fxml.Initializable;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
+    private Stage primaryStage;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    public void closeMainWindow() {
+        this.primaryStage.close();
+    }
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public void setPrimaryStage(Stage primaryStage) {
+        this.primaryStage = primaryStage;
     }
 }
